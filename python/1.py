1b4820311b6e50b5927a21a21f65d9e0c14f7965
theRampantRoadroller <104898729+theRampantRoadroller@users.noreply.github.com>
2024-01-16 10:58:12 +0530

    numbered serial seperate for each entity

--- python/1.py
+++ python/1.py
@@ -10,41 +10,53 @@ def id1(input_tag):
     else:
        return "00"
 
+def id2(input_tag):
+    y = input_tag % 26
+    x = int(input_tag / 26)
+    if x != 0 and y != 0:
+        return chr(96 + x) + chr(96 + y)
+    elif x == 0 and y != 0:
+        return " " +chr(96 + y)
+    elif y == 0 and x!=0:
+       return " " +"z"
+    else:
+       return "01"
 
 
-x=input("Enter Name\n").split(",")
-#print(x)
-with open("store1.txt","w") as f:
-    input_tag=0
-    for y in x:
-     if (input_tag%5==0):
-         f.write("\n")
-     f.write(" "+y+chr(2))
-     input_tag+=1
+def inputdata():
+    x=input("Enter Name\n").split(",")
+    #print(x)
+    with open("store1.txt","w") as f:
+        input_tag=0
+        for y in x:
+           if (input_tag%5==0):
+                f.write("\n")
+           f.write(y+chr(2))
+           input_tag+=1
+    f.close()
+    return len(x)
 
-print("A")
-f.close()
-my_list=['']*len(x)
+def loaddata():
+    with open("store1.txt","r") as f:
+        count=0
+        num=0
+        my_list[0]+=str(5)
+        for y in f.read():
+                
+                if (y=="\n"):
+                    my_list[count]+=id2(0)
+                    head=0
+                    my_list[count]+=str(head)
+                elif (y==chr(2)):
+                    count+=1
+                    if (len(my_list)<=count):
+                        my_list.append('')
+                    head+=1
+                    my_list[count]+=str(head)
+                else:
+                    my_list[count]+=y
 
-with open("store1.txt","r") as f:
-    count=0
-    num=0
-    for y in f.read():
-            
-            if (y==chr(2)):
-                count+=1
-                if(count>=len(my_list)):
-                    my_list.append('')
-            elif (y=="\n"):
-                my_list[count]+=id1(0)
-                last=1
-            elif (y==" "and last!=1):
-                my_list[count]+=id1(num)
-                num+=1
-                last=0
-            else:
-                my_list[count]+=y
-                last=0
-
+my_list=['']*inputdata()
+loaddata()
 for z in my_list:
     print(z)
